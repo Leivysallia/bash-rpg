a86749a4b05c4314da43fc8a554418a795e9bbc1
Leivysallia <85811527+Leivysallia@users.noreply.github.com>
2022-04-28 11:42:38 -0400

    bestiary update

--- seed.sh
+++ seed.sh
@@ -5,3 +5,13 @@ SEED=$(date +%s%N)
 RANDOM=$(date +%Y%m%d%I%M%S%N)
 
 RANDOM=$((RANDOM ** SEED))
+
+while IFS=: read -r name vit atk agi def exp syl; do
+	echo "NAME=$name" >> "$name"
+	echo "VIT=$vit" >> "$name"
+	echo "ATK=$atk" >> "$name"
+	echo "AGI=$agi" >> "$name"
+	echo "DEF=$def" >> "$name"
+	echo "EXP=$exp" >> "$name"
+	echo "SYL=$syl" >> "$name"
+done < bestiary
